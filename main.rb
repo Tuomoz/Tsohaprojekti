67d71ede1af418442d8130788172e8fbd642bce4
Tuomo Karkkainen <tuomon.email@gmail.com>
2012-11-11 23:40:17 +0200

    Toteutettu yksinkertainen kirjautuminen sisaan ja ulos

--- main.rb
+++ main.rb
@@ -1,11 +1,56 @@
 require 'sinatra'
 require "sinatra/reloader"
 require "sequel"
+require "haml"
+require 'rack-flash'
+
+
+DB = Sequel.connect('sqlite://testdb')
+use Rack::Flash
 
 configure do
   enable :sessions
 end
 
-get '/' do  
+helpers do
+  def logged_in?
+    session[:user] != nil
+  end
+
+  def get_nav_class path
+    if request.path_info == path
+      "active"
+    else
+      ""
+    end
+  end
+end
+
+get "/" do
+  if params.has_key?("logout")
+    session.clear
+  end
   haml :index
+end
+
+post "/" do
+
+  user = DB.fetch("SELECT id FROM users WHERE username = ? AND password = ?", 
+                  params[:username], params[:password]).first
+
+  if user
+    session[:user] = user[:id]
+    redirect "/user"
+  else
+    flash[:status_msg] = :login_error
+    redirect "/"
+  end
+end
+
+get "/user" do
+  if logged_in?
+    haml :user
+  else
+    redirect "/"
+  end
 end

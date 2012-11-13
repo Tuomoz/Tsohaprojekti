8e73df67051203e839dd8f6718a1350254826a79
Tuomo Karkkainen <tuomon.email@gmail.com>
2012-11-13 18:05:30 +0200

    Aikojen lisaaminen ja poistaminen mahdollista

--- main.rb
+++ main.rb
@@ -49,8 +49,34 @@ end
 
 get "/user" do
   if logged_in?
-    haml :user
+    times = DB.fetch("SELECT * FROM times WHERE user_id = ?", session[:user])
+    haml :user, locals: {times: times}
   else
+    flash[:status_msg] = :need_login
     redirect "/"
   end
+end
+
+post "/addtime" do
+  unless logged_in?
+    flash[:status_msg] = :need_login
+    redirect "/"
+  end
+
+  insert_ds = DB["INSERT INTO times VALUES (null, ?, ?, ?, ?)", 
+                params[:date], params[:time_start], params[:time_end], session[:user]]
+  insert_ds.insert
+  flash[:status_msg] = :time_added
+  redirect "/user"
+end
+
+get "/deletetime/:timeid" do
+  unless logged_in?
+    flash[:status_msg] = :need_login
+    redirect "/"
+  end
+
+  delete_ds = DB["DELETE FROM times WHERE id = ? AND user_id = ?", params[:timeid], session[:user]]
+  delete_ds.delete
+  redirect "/user"
 end
